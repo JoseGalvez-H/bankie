06945e9d76a5349a503f5f821d9c525c642db082
Jose Galvez-Hernandez <josemgalvezh90@gmail.com>
2024-07-12 16:07:48 -0700

    Add icons to sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -28,9 +28,19 @@ const Sidebar = ({ user }: SiderbarProps) => {
 
                     return (
                         <Link href={item.route} key={item.label}
-                        className={cn('sidebar-link', {'bg-bank-gradient': isActive})}
+                            className={cn('sidebar-link', { 'bg-bank-gradient': isActive })}
                         >
-                            {item.label}
+                            <div className="relative size-6">
+                                <Image
+                                    src={item.imgURL}
+                                    alt={item.label}
+                                    fill
+                                    className={cn({
+                                        'brightness-[3] invert-0': isActive
+                                    })}
+                                />
+
+                            </div>
                         </Link>
                     )
                 })}
